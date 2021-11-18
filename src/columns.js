d7ad91bf89a0b0b15423655e5f1e2a574f4f1f81
Eric Fung <eskfung@gmail.com>
2021-11-18 13:29:32 -0500

    Define table columns independently

---
+++ src/columns.js
@@ -0,0 +1,30 @@
+export const columns = [
+  {
+    Header: 'Name',
+    accessor: 'name',
+  },
+  {
+    Header: 'Party Pack',
+    accessor: 'pack',
+  },
+  {
+    Header: "Players",
+    accessor: "maxPlayers",
+  },
+  {
+    Header: "Family Friendly Setting",
+    accessor: "familyFriendlySetting",
+  },
+  {
+    Header: "Manual Censoring",
+    accessor: "manualCensoring",
+  },
+  {
+    Header: "Extended Timers",
+    accessor: "extendedTimers",
+  },
+  {
+    Header: "Description",
+    accessor: "description",
+  }
+]
