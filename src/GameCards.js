6ff9f9959d652abfe1ab663d90f4401bd9b92d93
Brian Kelly <polymonic@gmail.com>
2022-05-08 17:47:56 -0500

    Actually use Bootstrap React components and clean up spacing

--- src/GameCards.js
+++ src/GameCards.js
@@ -1,75 +1,73 @@
 import React from "react";
 import {
-  faChild,
+  faChildReaching,
   faHourglass,
   faCommentSlash,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Card, Col, Row, Stack } from "react-bootstrap";
 
 const GameCards = ({ games }) => {
   return (
-    <div className="row row-cols-1 row-cols-md-3 g-4 mb-4">
+    <Row xs={1} sm={2} md={2} lg={3} xl={3} xxl={3}>
+      {/*<div className="row row-cols-1 row-cols-md-3 g-4 mb-4">*/}
       {games.map((game) => {
         return (
-          <div className="col" key={game.name}>
-            <div className="card mb-2">
+          <Col key={game.name}>
+            <Card className="mb-4 shadow">
               {game.image && (
                 <a href={game.url}>
-                  <img
-                    src={`/images/${game.image}`}
-                    className="card-img-top"
-                    alt={game.name}
-                  />
+                  <Card.Img variant="top" src={`/images/${game.image}`} />
                 </a>
               )}
-              <div className="card-body">
-                <h5 className="card-title">
+              <Card.Body>
+                <Card.Title>
                   <a href={game.url} className="text-decoration-none">
                     {game.name}
                   </a>
-                </h5>
-                <h6 className="card-subtitle text-muted">
+                </Card.Title>
+                <Card.Subtitle className="text-muted mb-2">
                   {game.minPlayers} - {game.maxPlayers} players
-                </h6>
-                <p className="card-text">{game.description}</p>
-              </div>
-              <ul className="list-group list-group-flush">
-                {game.familyFriendlySetting && (
-                  <li className="list-group-item">
-                    <FontAwesomeIcon icon={faChild} fixedWidth /> Family
-                    Friendly Setting
-                  </li>
-                )}
-                {game.manualCensoring && (
-                  <li className="list-group-item">
-                    <FontAwesomeIcon icon={faCommentSlash} fixedWidth /> Manual
-                    Censoring
-                  </li>
-                )}
-                {game.extendedTimers && (
-                  <li className="list-group-item">
-                    <FontAwesomeIcon icon={faHourglass} fixedWidth /> Extended
-                    Timers
-                  </li>
-                )}
-              </ul>
-              <div className="card-footer">
+                </Card.Subtitle>
+                <Card.Text>{game.description}</Card.Text>
+                <Stack direction="vertical" gap={1}>
+                  {game.familyFriendlySetting && (
+                    <div>
+                      <FontAwesomeIcon icon={faChildReaching} fixedWidth />{" "}
+                      Family Friendly Setting
+                    </div>
+                  )}
+                  {game.manualCensoring && (
+                    <div>
+                      <FontAwesomeIcon icon={faCommentSlash} fixedWidth />{" "}
+                      Manual Censoring
+                    </div>
+                  )}
+                  {game.extendedTimers && (
+                    <div>
+                      <FontAwesomeIcon icon={faHourglass} fixedWidth /> Extended
+                      Timers
+                    </div>
+                  )}
+                </Stack>
+              </Card.Body>
+              <Card.Footer>
                 {game.pack_url ? (
-                  <span>
+                  <>
                     Part of{" "}
                     <a href={game.pack_url} className="text-decoration-none">
                       {game.pack}
                     </a>
-                  </span>
+                  </>
                 ) : (
-                  <span>{game.pack}</span>
+                  <>{game.pack}</>
                 )}
-              </div>
-            </div>
-          </div>
+              </Card.Footer>
+            </Card>
+          </Col>
         );
       })}
-    </div>
+    </Row>
   );
 };
 
