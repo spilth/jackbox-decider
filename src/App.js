3e5d6419abfca1b22f74cc32af21c6c64e4c1aee
vladyslav.suprunov <vladyslav.suprunov@pm.bet>
2022-05-30 11:53:00 +0300

    Rename russian label

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
   ];
   const languageButtons = [
     { label: "English", value: "en" },
-    { label: "Russian", value: "ru" },
+    { label: "Русский", value: "ru" },
     { label: "Українська", value: "ua" },
   ];
 
